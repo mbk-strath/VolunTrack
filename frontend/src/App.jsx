8c40da82cb81d7d04d9f3117eb36da6e48de9cc0
lindaopolo7896 <eleanor.opolo@strathmore.edu>
2025-10-12 14:16:45 +0300

    Fixeds some React issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/main/LandingPage";
 import Login from "./pages/main/Login";
--- frontend/src/components/volunteer/Search.jsx
+++ frontend/src/components/volunteer/Search.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FaSearch, FaFilter } from "react-icons/fa";
+import React from "react";
 
 function Search({ onSearch, onFilter }) {
   const [query, setQuery] = useState("");

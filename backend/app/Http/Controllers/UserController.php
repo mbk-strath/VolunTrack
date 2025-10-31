37014d96ce3df13af2863afc90dc0c6a22fb313f
Petermondo <peter.obwogi@strathmore.edu>
2025-10-31 12:12:24 +0300

    register moved to user controller

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -25,6 +25,63 @@ class UserController extends Controller
         return response()->json($users);
     }
 
-    
+public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'gender' => 'required|string|max:255',
+            'role' => 'required|string|in:organisation,volunteer,admin',
+        ]);
+        $name = $request->name;
+        $email = $request->email;
+        $phone = $request->phone;
+        $gender = $request->gender;
+        $password = $request->password;
+        $role = $request->role;
+
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password),
+            'gender' => $gender,
+            'phone' => $phone,
+            'role' => $role,
+        ]);
+
+        if (!$user) {
+            return response()->json(['message' => 'Registration failed'], 500);
+        }
+
+        if ($role === 'admin'){
+            $user->is_verified = true;
+            $user->is_active = true;
+            $user->save();
+            return response()->json(['message' => 'Admin registration successful.'], 201);
+        }
+
+       if ($role === 'organisation') {
+
+
+            Organisation::create([
+                'user_id' => $user->id,
+            ]);
+
+        } elseif ($role === 'volunteer') {
+
+            Volunteer::create([
+                'user_id' => $user->id,
+            ]);
+        }
+        else{
+            return response()->json(['message' => 'Invalid role'], 400);
+        }
+
+        $link = url("/api/verify/{$user->id}"); 
+        Mail::to($email)->send(new VerificationEmail($link));   
+        return response()->json(['user' => $user, 'message' => 'Registration successful. Please check your email for verification instructions.'], 201);
+
+    }
 
 }

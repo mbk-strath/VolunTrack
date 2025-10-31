b8e7043c53f4464899ffc2c3f4d8d557100489bf
Petermondo <peter.obwogi@strathmore.edu>
2025-10-31 12:04:41 +0300

    user list

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+//Databases
+use App\Models\User;
+use App\Models\Organisation;
+use App\Models\Volunteer;
+use App\Models\OtpCode;
+
+//Resources
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\VerificationEmail;
+use App\Mail\OtpEmail;
+use App\Mail\PasswordResetEmail;
+use App\Services\MembershipService;
+
+class UserController extends Controller
+{
+    public function list()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    
+}

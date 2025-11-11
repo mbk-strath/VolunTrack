88ae3855a6a346ce41af6ea5260e2fa45ba80e4d
Varriable <ramadhanabdilatif13@gmail.com>
2025-11-11 08:38:36 +0300

    applicantion and volunteer name

--- backend/app/Models/Application.php
+++ backend/app/Models/Application.php
@@ -15,4 +15,16 @@ class Application extends Model
         'application_date',
         'status'
     ];
+
+    protected $appends = ['volunteer_name', 'opportunity_title'];
+    public function getVolunteerNameAttribute()
+    {
+        $volunteer = Volunteer::find($this->volunteer_id);
+        return $volunteer ? $volunteer->name : null;
+    }
+    public function getOpportunityTitleAttribute()
+    {
+        $opportunity = Opportunity::find($this->opportunity_id);
+        return $opportunity ? $opportunity->title : null;
+    }
 }
